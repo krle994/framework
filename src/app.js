972ad0da208b7ac68d61b3120e600e1b1055a318
Milan <krlekrstic018@gmail.com>
2018-05-04 16:55:49 +0200

    Added functionality for the virtual DOM for adding and updating props

--- src/app.js
+++ src/app.js
@@ -2,39 +2,41 @@ import {
   h,
   createElement,
   changed,
-  updateElement
+  updateElement,
+  isCustomProp,
+  setProp,
+  setProps,
+  updateProp,
+  updateProps,
+  setBooleanProp,
+  removeProp,
+  removeBooleanProp
 } from './virtualDom';
 
-
-const a = (
-  <ul>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 1</li>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 1</li>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 1</li>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 1</li>
+const f = (
+  <ul style="list-style: none;">
+    <li className="item">item 1</li>
+    <li className="item">
+      <input type="checkbox" checked={true} />
+      <input type="text" disabled={false} />
+    </li>
   </ul>
 );
 
-
-const b = (
-  <ul>
-    <li>item 1</li>
-    <li>hello!</li>
+const g = (
+  <ul style="list-style: none;">
+    <li className="item item2">item 1</li>
+    <li style="background: red;">
+      <input type="checkbox" checked={false} />
+      <input type="text" disabled={true} />
+    </li>
   </ul>
 );
 
 const $root = document.getElementById('root');
 const $reload = document.getElementById('reload');
 
-updateElement($root, a);
+updateElement($root, f);
 $reload.addEventListener('click', () => {
-  updateElement($root, b, a);
+  updateElement($root, g, f);
 });
--- src/virtualDOM.js
+++ src/virtualDOM.js
@@ -10,15 +10,79 @@ export function h(type, props, ...children) {
   };
 }
 
+export function isCustomProp() {
+  return false;
+}
+
+export function setProp(target, name, value) {
+  if(isCustomProp(name)) {
+    return;
+  } else if (name === 'className') {
+    target.setAttribute('class', value);
+  } else if (typeof value === 'boolean') {
+    setBooleanProp(target, name, value);
+  } else {
+    target.setAttribute(name, value);
+  }
+}
+
+export function setProps(target, props) {
+  Object.keys(props).forEach(name => {
+    setProp(target, name, props[name]);
+  });
+}
+
+export function updateProp(target, name, newValue, oldValue) {
+  if (!newValue) {
+    removeProp(target, name, oldValue);
+  } else if (!oldValue) {
+    setProp(target, name, newValue);
+  }
+}
+
+export function updateProps(target, newProps, oldProps = {}) {
+  const props = Object.assign({}, newProps, oldProps);
+  Object.keys(props).forEach(name => {
+    updateProp(target, name, newProps[name], oldProps[name]);
+  });
+}
+
+export function setBooleanProp(target, name, value) {
+  if (value) {
+    target.setAttribute(name, value);
+    target[name] = true;
+  } else {
+    target[name] = false;
+  }
+}
+
+export function removeProp(target, name, value) {
+  if(isCustomProp(name)) {
+    return;
+  } else if (name === 'className') {
+    target.removeAttribute('class');
+  } else if (typeof value === 'boolean') {
+    removeBooleanProp(target, name);
+  } else {
+    target.removeAttribute(name, value);
+  }
+}
+
+export function removeBooleanProp(target, name) {
+  target.removeAttribute(name);
+  target[name] = false;
+}
+
 export function createElement(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
   }
-  const $el = document.createElement(node.type);
+  const elem = document.createElement(node.type);
+  setProps(elem, node.props)
   node.children
     .map(createElement)
-    .forEach($el.appendChild.bind($el));
-  return $el;
+    .forEach(elem.appendChild.bind(elem));
+  return elem;
 }
 
 export function changed(node1, node2) {
@@ -42,6 +106,11 @@ export function updateElement(parent, newNode, oldNode, index = 0) {
       parent.childNodes[index]
     );
   } else if (newNode.type) {
+    updateProps(
+      parent.childNodes[index],
+      newNode.props,
+      oldNode.props
+    );
     const newLength = newNode.children.length;
     const oldLength = oldNode.children.length;
     for (let i = 0; i < newLength || i < oldLength; i++) {
